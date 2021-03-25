db4b8c3da78c1c8b8e1e206f2584aaa2daa850f0
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-03-25 08:23:52 +0300

    feat: добавил стили к форме регистрации

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,10 @@
+import SignUp from './SignUp'
+
 function App() {
   return (
-    <div className='sign-up'>
-      <div className='sign-up__content'>
-        <form>
-          <p className='sign-up__text'>
-            <span className='sign-up__text-extra'>login as</span>:USER_EMAIL=
-            <input className="sign-up__input" 
-              type="email" 
-              minLength="5" 
-              maxLength="40" 
-              placeholder="Email" 
-              required
-            />          
-          </p>
-          <p className='sign-up__text'>
-            <span className='sign-up__text-extra'>Password</span>:PASSWORD=
-            <input className="sign-up__input" 
-                type="password" 
-                minLength="5" 
-                maxLength="40" 
-                placeholder="Password" 
-                required
-              />
-          </p>
-          <p className='sign-up__text'>
-            <span className='sign-up__text-extra'>[confirm@button]$</span>
-            <button className='sign-up__submit' type='submit'>enter</button>
-          </p>
-        </form>
-      </div>
-    </div>
+    <>
+      <SignUp />
+    </>
   );
 }
 
